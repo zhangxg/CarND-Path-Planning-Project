4cd21856646913da1745d201b97951c980fa8831
zhangxiaogang <zhangxiaogang@xiaomi.com>
2017-10-25 19:02:11 +0800

    add spline, now moving

--- src/main.cpp
+++ src/main.cpp
@@ -39,6 +39,10 @@ double distance(double x1, double y1, double x2, double y2)
 {
 	return sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
 }
+
+// the x, y are the location of the car. 
+// the mapx and mapy are the map points list.
+
 int ClosestWaypoint(double x, double y, const vector<double> &maps_x, const vector<double> &maps_y)
 {
 
@@ -62,6 +66,15 @@ int ClosestWaypoint(double x, double y, const vector<double> &maps_x, const vect
 
 }
 
+
+// how to write comments; google comment code style: https://google.github.io/styleguide/cppguide.html#Comments
+
+// the x, y are the car's location. 
+// what's the theta?
+// todo: explain this with a graph.
+// todo: why the next way points is calcualted? by considering the heading direction of the car, this will get the next way point the car will drive to, this is necessary because the nearest point may be behin the car, and we don't want the car to drive back. 
+
+
 int NextWaypoint(double x, double y, double theta, const vector<double> &maps_x, const vector<double> &maps_y)
 {
 
@@ -70,7 +83,8 @@ int NextWaypoint(double x, double y, double theta, const vector<double> &maps_x,
 	double map_x = maps_x[closestWaypoint];
 	double map_y = maps_y[closestWaypoint];
 
-	double heading = atan2( (map_y-y),(map_x-x) );
+  // this is the car's driving direction. 
+	double heading = atan2((map_y-y), (map_x-x));
 
 	double angle = abs(theta-heading);
 
@@ -133,6 +147,7 @@ vector<double> getFrenet(double x, double y, double theta, const vector<double> 
 }
 
 // Transform from Frenet s,d coordinates to Cartesian x,y
+// todo: explain how? 
 vector<double> getXY(double s, double d, const vector<double> &maps_s, const vector<double> &maps_x, const vector<double> &maps_y)
 {
 	int prev_wp = -1;
@@ -160,9 +175,12 @@ vector<double> getXY(double s, double d, const vector<double> &maps_s, const vec
 
 }
 
-int main() {
+int main() 
+{
   uWS::Hub h;
 
+
+  // todo: moving the file parsing to a function, maybe a file. 
   // Load up map values for waypoint's x,y,s and d normalized normal vectors
   vector<double> map_waypoints_x;
   vector<double> map_waypoints_y;
@@ -197,8 +215,12 @@ int main() {
   	map_waypoints_dy.push_back(d_y);
   }
 
-  h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,&map_waypoints_dx,&map_waypoints_dy](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
-                     uWS::OpCode opCode) {
+  // xg: the socket package 
+  int lane = 1;
+
+  double ref_vel = 49.5;
+
+  h.onMessage([&ref_vel, &map_waypoints_x,&map_waypoints_y,&map_waypoints_s,&map_waypoints_dx,&map_waypoints_dy, &lane](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
     // The 4 signifies a websocket message
     // The 2 signifies a websocket event
@@ -212,76 +234,213 @@ int main() {
         auto j = json::parse(s);
         
         string event = j[0].get<string>();
-        
+        // xg: for now, the simulator gives us the localization data, 
+        // in the practice, this will de determined by localization modular.  
         if (event == "telemetry") {
           // j[1] is the data JSON object
-          
         	// Main car's localization Data
-          	double car_x = j[1]["x"];
-          	double car_y = j[1]["y"];
-          	double car_s = j[1]["s"];
-          	double car_d = j[1]["d"];
-          	double car_yaw = j[1]["yaw"];
-          	double car_speed = j[1]["speed"];
+        	double car_x = j[1]["x"];
+        	double car_y = j[1]["y"];
+        	double car_s = j[1]["s"];
+        	double car_d = j[1]["d"];
+        	double car_yaw = j[1]["yaw"];
+        	double car_speed = j[1]["speed"];
 
-          	// Previous path data given to the Planner
-          	auto previous_path_x = j[1]["previous_path_x"];
-          	auto previous_path_y = j[1]["previous_path_y"];
-          	// Previous path's end s and d values 
-          	double end_path_s = j[1]["end_path_s"];
-          	double end_path_d = j[1]["end_path_d"];
+        	// Previous path data given to the Planner
+        	auto previous_path_x = j[1]["previous_path_x"];
+        	auto previous_path_y = j[1]["previous_path_y"];
+        	// Previous path's end s and d values 
+        	double end_path_s = j[1]["end_path_s"];
+        	double end_path_d = j[1]["end_path_d"];
 
-          	// Sensor Fusion Data, a list of all other cars on the same side of the road.
-          	auto sensor_fusion = j[1]["sensor_fusion"];
+        	// Sensor Fusion Data, a list of all other cars on the same side of the road.
+        	auto sensor_fusion = j[1]["sensor_fusion"];
 
-          	json msgJson;
+          // =====  above is the provided code 
 
-          	vector<double> next_x_vals;
-          	vector<double> next_y_vals;
+          int prev_size = previous_path_x.size();
+
+          std::vector<double> ptsx;
+          std::vector<double> ptsy;
+
+          double ref_x = car_x;
+          double ref_y = car_y;
+          //xg-todo: when to convert to radius, when to use degree? 
+          double ref_yaw = deg2rad(car_yaw);
+          // cout << "the car's parameter" << endl;
+          // cout << car_x << ", " << ", " << car_y << ", " << car_yaw << endl;
+
+          // the points are used to generate the spline, 
+          // if the value is less than 2 (zero or 1), it's hard to make the 
+          // the spline. we at least give it two. 
+          // cout << "vaues in list " << prev_size << endl;
+          if (prev_size < 2) 
+          {
+            // xg-todo: use degree
+            double prev_car_x = car_x - cos(car_yaw);
+            double prev_car_y = car_y - sin(car_yaw);
+
+            ptsx.push_back(prev_car_x);
+            ptsx.push_back(car_x);
+
+            ptsy.push_back(prev_car_y);
+            ptsy.push_back(car_y);
+          }
+          // if the returned previous path contains more than 2 points, 
+          // we will leverage them. 
+          else 
+          {
+            // the last point the car used. 
+            ref_x = previous_path_x[prev_size - 1];
+            ref_y = previous_path_y[prev_size - 1];
+
+            // get the next last point used by the car
+            double ref_x_prev = previous_path_x[prev_size - 2];
+            double ref_y_prev = previous_path_y[prev_size - 2];
+            // get the car's heading (angel) 
+            // but it's not used ? 
+            // xg-todo: that's the atan2() function, it used a lot;
+            ref_yaw = atan2(ref_y - ref_y_prev, ref_x - ref_x_prev);
+
+            ptsx.push_back(ref_x_prev);
+            ptsx.push_back(ref_x);
+
+            ptsy.push_back(ref_y_prev);
+            ptsy.push_back(ref_y);
+          }
+
+          vector<double > next_wp0 = getXY(car_s+30, (2+4*lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+          vector<double > next_wp1 = getXY(car_s+60, (2+4*lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+          vector<double > next_wp2 = getXY(car_s+90, (2+4*lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+
+          ptsx.push_back(next_wp0[0]);
+          ptsx.push_back(next_wp1[0]);
+          ptsx.push_back(next_wp2[0]);
+
+          ptsy.push_back(next_wp0[1]);
+          ptsy.push_back(next_wp1[1]);
+          ptsy.push_back(next_wp2[1]);
 
 
-          	// TODO: define a path made up of (x,y) points that the car will visit sequentially every .02 seconds
-            //xg: getting started code:
-            double dist_inc = 0.5;
-            
-            // std::vector<double> f_start = getFrenet(car_x, car_y, car_yaw, map_waypoints_x, map_waypoints_y); 
-            // cout << f_start[0] << "," << f_start[1] << endl;
-            // std::vector<double> c_xy; 
-            // cout << "moving ... " << endl;
+          //transfer the cartisian coordinates to the car's coordinates
+          for (int i = 0; i < ptsx.size(); ++i)
+          {
+            double shift_x = ptsx[i] - ref_x;
+            double shift_y = ptsy[i] - ref_y;
 
-            for(int i = 0; i < 50; i++)
-            {
-              // 1.  driving a straight line
-              // next_x_vals.push_back(car_x+(dist_inc*i)*cos(deg2rad(car_yaw)));
-              // next_y_vals.push_back(car_y+(dist_inc*i)*sin(deg2rad(car_yaw)));
+            double car_angel = 0 - ref_yaw;
+            ptsx[i] = shift_x * cos(car_angel) - shift_y * sin(car_angel);
+            ptsy[i] = shift_x * sin(car_angel) + shift_y * cos(car_angel);
+          }
 
-              // 2. stay in the line, using frenet coordinates
-              // the getXY() methods requires s, x, y, not x, y, s, the later can not move. 
-              // this now moves, but osscilate a lot. 
-              // c_xy = getXY(f_start[0]+dist_inc*i, f_start[1], map_waypoints_s, map_waypoints_x, map_waypoints_y);
+          // introduce the spline
+          tk::spline s;
 
-              // next_x_vals.push_back(c_xy[0]);
-              // next_y_vals.push_back(c_xy[1]);
+          s.set_points(ptsx, ptsy);
 
-              // above my work, not working. why? 
-              double next_s = car_s + (i + 1) * dist_inc;
-              double next_d = 6;
-              std::vector<double> xy = getXY(next_s, next_d, map_waypoints_s, map_waypoints_x, map_waypoints_y);
+          // the new waypoints sent to the car
+        	vector<double> next_x_vals;
+        	vector<double> next_y_vals;
 
-              next_x_vals.push_back(xy[0]);
-              next_y_vals.push_back(xy[1]);              
-            }
-/*
+          // reuse the previous path;
+          // don't the already be used? not, they are the leftovers. 
+          // the simulator makes sure only the leftovers returned.
+          for (int i = 0; i < previous_path_x.size(); ++i)
+          {
+            next_x_vals.push_back(previous_path_x[i]);
+            next_y_vals.push_back(previous_path_y[i]);
+          }
 
-            // coding stops here; 
+          // calculate the points from the spline
+          double target_x = 30.0;
+          double target_y = s(target_x);
+          // we used the car's coordinates
+          double target_dist = sqrt(target_x*target_x + target_y*target_y);
 
-          	msgJson["next_x"] = next_x_vals;
-          	msgJson["next_y"] = next_y_vals;
+          double x_add_on = 0;
 
-          	auto msg = "42[\"control\","+ msgJson.dump()+"]";
+          for (int i = 0; i <= 50 - previous_path_x.size(); ++i)
+          {
+            double N = target_dist / (0.02 * ref_vel / 2.24);
+            double x_point = x_add_on + target_x / N;
+            double y_point = s(x_point);
 
-          	//this_thread::sleep_for(chrono::milliseconds(1000));
-          	ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
+            // for next use
+            x_add_on = x_point;
+
+            // x_point and y_point are in car's coordinate
+            double x_ref = x_point;
+            double y_ref = y_point;
+
+            // convert back to normal coordinates
+            // rotate back
+            x_point = x_ref * cos(ref_yaw) - y_ref * sin(ref_yaw);
+            y_point = x_ref * sin(ref_yaw) + y_ref * cos(ref_yaw);
+
+            // translate back
+            // bug: if set this way, the car doen't move.  see: 
+            // https://discussions.udacity.com/t/walkthrough-video-code-problem/376665/7
+
+            // x_point += x_ref;
+            // y_point += y_ref;
+
+
+            x_point += ref_x;
+            y_point += ref_y;
+
+            //add the new way points
+            next_x_vals.push_back(x_point);
+            next_y_vals.push_back(y_point);
+          }
+
+          // cout << " the points in the list " << endl;
+          // for (int i = 0; i < next_x_vals.size(); ++i)
+          // {
+          //   cout << next_x_vals[i] << ", " << next_y_vals[i] << endl;
+          // }
+
+          // cout << next_x_vals.size() << endl;
+        	// // TODO: define a path made up of (x,y) points that the car will visit sequentially every .02 seconds
+         //  //xg: getting started code:
+         //  double dist_inc = 0.5;
+          
+         //  // std::vector<double> f_start = getFrenet(car_x, car_y, car_yaw, map_waypoints_x, map_waypoints_y); 
+         //  // cout << f_start[0] << "," << f_start[1] << endl;
+         //  // std::vector<double> c_xy; 
+         //  // cout << "moving ... " << endl;
+
+         //  for(int i = 0; i < 50; i++)
+         //  {
+         //    // 1.  driving a straight line
+         //    // next_x_vals.push_back(car_x+(dist_inc*i)*cos(deg2rad(car_yaw)));
+         //    // next_y_vals.push_back(car_y+(dist_inc*i)*sin(deg2rad(car_yaw)));
+
+         //    // 2. stay in the line, using frenet coordinates
+         //    // the getXY() methods requires s, x, y, not x, y, s, the later can not move. 
+         //    // this now moves, but osscilate a lot. 
+         //    // c_xy = getXY(f_start[0]+dist_inc*i, f_start[1], map_waypoints_s, map_waypoints_x, map_waypoints_y);
+
+         //    // next_x_vals.push_back(c_xy[0]);
+         //    // next_y_vals.push_back(c_xy[1]);
+
+         //    // above my work, not working. why? 
+         //    double next_s = car_s + (i + 1) * dist_inc;
+         //    double next_d = 6;
+         //    std::vector<double> xy = getXY(next_s, next_d, map_waypoints_s, map_waypoints_x, map_waypoints_y);
+
+         //    next_x_vals.push_back(xy[0]);
+         //    next_y_vals.push_back(xy[1]);              
+         //  }
+          // cout << next_x_vals[0] << ", " << next_y_vals[0] << endl;
+          // STOP
+          json msgJson;
+        	msgJson["next_x"] = next_x_vals;
+        	msgJson["next_y"] = next_y_vals;
+
+        	auto msg = "42[\"control\","+ msgJson.dump()+"]";
+
+        	//this_thread::sleep_for(chrono::milliseconds(1000));
+        	ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
           
         }
       } else {
@@ -325,3 +484,17 @@ int main() {
   }
   h.run();
 }
+
+
+/**
+========================
+the errors:
+variable 'lane' cannot be implicitly captured in a lambda with no capture-default specified
+          vector<double > next_wp1 = getXY(car_s+60, (2+4*lane), map_waypoints_s, map_waypo...
+
+https://stackoverflow.com/questions/30217956/error-variable-cannot-be-implicitly-captured-because-no-default-capture-mode-h/30217989
+
+
+========================
+
+*/
